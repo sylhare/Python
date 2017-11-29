19356987c1cf04c0319c50c417f79e09bec224e0
sylhare <sylhareng@gmail.com>
2017-11-29 16:23:45 -0500

    added examples

---
+++ src/examples/math.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  2 09:17:36 2016
+
+@author: sylhare
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+x1 = np.arange(-5,5,1)
+y1 = np.arange(-5,5,0.5)
+
+def disc(x,y):
+    return (1+x)**2+(1+y)**2
+
+def cone(x,y,z):
+    return np.sqrt((1+x)**2+(1+y)**2)
+    
+x, y = np.mgrid[-20:20:10j, -20:20:10j]
+z=disc(x,y)
+plt.figure(1)
+plt.subplot(211)
+plt.contour(x, y, z, colors='k') 
+plt.show()
+
+
+#--------------------------------------
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+X = np.arange(-30, 30, 0.1)
+Y = np.arange(-30, 30, 0.1)
+X, Y = np.meshgrid(X, Y)
+Z = np.log (X**2 + Y**2)
+
+#3D plot; coolwarm for the color
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+ax.set_zlim(0, 6)
+
+#ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+ 
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.show()
