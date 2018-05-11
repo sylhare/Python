1f46298b405dd140e18243d42a87da576f99ad7a
sylhare <sylhareng@gmail.com>
2018-05-11 12:56:14 -0400

    Add swagger server

---
+++ src/projects/ContactStore/python-flask-server/swagger_server/controllers/contact_controller.py
@@ -0,0 +1,76 @@
+import connexion
+import six
+
+from swagger_server.models.contact import Contact  # noqa: E501
+from swagger_server import util
+
+
+def add_contact(body=None):  # noqa: E501
+    """Add a new contact
+
+    description for add new contact # noqa: E501
+
+    :param body: Contact object that needs to be added to the store
+    :type body: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        body = Contact.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def delete_contact(contactId):  # noqa: E501
+    """Deletes a contact
+
+     # noqa: E501
+
+    :param contactId: Contact id to delete
+    :type contactId: int
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def get_contact_by_id(contactId):  # noqa: E501
+    """Find contact by ID
+
+    Returns a contact # noqa: E501
+
+    :param contactId: ID of contact that needs to be fetched
+    :type contactId: int
+
+    :rtype: Contact
+    """
+    return 'do some magic!'
+
+
+def get_contacts(limit=None):  # noqa: E501
+    """list of all contact
+
+    Returns list of contact # noqa: E501
+
+    :param limit: limit of return
+    :type limit: int
+
+    :rtype: List[Contact]
+    """
+    return 'do some magic!'
+
+
+def update_contact(contactId, body=None):  # noqa: E501
+    """Updates a contact in the store with form data
+
+     # noqa: E501
+
+    :param contactId: ID of contact that needs to be updated
+    :type contactId: str
+    :param body: Contact object that needs to be added to the store
+    :type body: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        body = Contact.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
---
+++ src/projects/ContactStore/python-flask-server/swagger_server/test/test_contact_controller.py
@@ -0,0 +1,84 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.contact import Contact  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestContactController(BaseTestCase):
+    """ContactController integration test stubs"""
+
+    def test_add_contact(self):
+        """Test case for add_contact
+
+        Add a new contact
+        """
+        body = Contact()
+        response = self.client.open(
+            '/v1/contacts',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_delete_contact(self):
+        """Test case for delete_contact
+
+        Deletes a contact
+        """
+        response = self.client.open(
+            '/v1/contact/{contactId}'.format(contactId=789),
+            method='DELETE',
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_contact_by_id(self):
+        """Test case for get_contact_by_id
+
+        Find contact by ID
+        """
+        response = self.client.open(
+            '/v1/contact/{contactId}'.format(contactId=789),
+            method='GET',
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_contacts(self):
+        """Test case for get_contacts
+
+        list of all contact
+        """
+        query_string = [('limit', 10000)]
+        response = self.client.open(
+            '/v1/contacts',
+            method='GET',
+            content_type='application/json',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_update_contact(self):
+        """Test case for update_contact
+
+        Updates a contact in the store with form data
+        """
+        body = Contact()
+        response = self.client.open(
+            '/v1/contact/{contactId}'.format(contactId='contactId_example'),
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
