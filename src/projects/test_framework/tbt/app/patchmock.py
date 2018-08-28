3f350bd06c9b26b982e6900e446e4bcdde0240f4
sylhare <sylhareng@gmail.com>
2018-08-28 10:18:58 -0400

    Update test framework

--- src/projects/test_framework/tbt/app/patchmock.py
+++ src/projects/test_framework/tbt/app/patchmock.py
@@ -2,14 +2,21 @@ import unittest
 import mock
 import sys
 
-
-
 if __name__ == '__main__':
+
     with mock.patch.dict(sys.modules, {'os': None}):
         try:
             import os
         except ImportError:
             print("Impossible, there is no python without os module!")
-    import os
 
+    import os
     print(os.getpid())
+
+    with mock.patch.dict(os.environ, clear=True):
+        try:
+            print(os.environ["PYTHONPATH"])
+        except KeyError:
+            print("mock an empty os.environ")
+
+    print(os.environ["PYTHONPATH"])
---
+++ src/projects/test_framework/tbt/tests/test_raise.py
@@ -0,0 +1,50 @@
+from ..app.raise_app import *
+import unittest
+
+
+def lambda_raise_param():
+    return lambda x: raise_if_None(x), None
+
+class TestRaise(unittest.TestCase):
+
+    def test_direct_raise_1(self):
+        self.assertRaises(TypeError, raise_directly)
+
+    @unittest.expectedFailure
+    def test_direct_raise_1_2(self):
+        self.assertRaises(TypeError, raise_directly())
+
+    def test_direct_raise_2(self):
+        with self.assertRaises(TypeError):
+            raise_directly()
+
+    @unittest.expectedFailure
+    def test_direct_raise_3(self):
+        self.assertRaises(KeyError, raise_directly)
+
+    def test_raise_with_param_1(self):
+        self.assertRaises(ValueError, raise_if_None(None))
+
+    @unittest.expectedFailure
+    def test_raise_with_param_1_2(self):
+        self.assertRaises(ValueError, raise_if_None, None)
+
+    @unittest.expectedFailure
+    def test_raise_with_param_2_1(self):
+        with self.assertRaises(ValueError):
+            lambda_raise_param()
+
+    @unittest.expectedFailure
+    def test_raise_with_param_2_2(self):
+        with self.assertRaises(ValueError):
+            raise_if_None(None)
+
+    def test_direct_raise_catched(self):
+        self.assertRaises(TypeError, raise_directly)
+
+    def test_raise_with_param_catched(self):
+        self.assertRaises(ValueError, raise_if_None(None))
+
+
+if __name__ == '__main__':
+    unittest.main()
