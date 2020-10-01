ed4d152e159d87f4a7298da0828a93e3b733615d
sylhare <sylhare@outlook.com>
2020-10-01 09:36:43 -0400

    Add minSum example

--- src/projects/Interview/questions/hacker_rank.py
+++ src/projects/Interview/questions/hacker_rank.py
@@ -53,3 +53,40 @@ def mid_angle(ab, bc):
     Returns the MBC angle
     """
     return round(math.degrees(math.atan(float(ab) / bc)))
+
+
+def reverse_sort_insert(result, e):
+    """ To insert in the list while keeping it reverse sorted"""
+    for count, number in enumerate(result):
+        if number < e:
+            result.insert(count, e)
+            return result
+
+    result.append(e)
+    return result
+
+
+def divide_k_biggest_ones(result, k):
+    """ to divide the biggest number even when the biggest has already been divided """
+    while k > 0:
+        # print("result {} k {}".format(result, k))
+        divided = math.ceil(result.pop(0) / 2.0)
+        result = reverse_sort_insert(result, divided)
+        k = k - 1
+
+    return sum(result)
+
+
+def minSum(num, k):
+    """ gives the minimal sum of a list after k number of divisions """
+    result = []
+    min_sum = 0
+    for e in num:
+        result = reverse_sort_insert(result, e)
+        # print(result)
+
+        if len(result) > k:
+            min_sum += result.pop(len(result) - 1)
+            # print("min_sum {} result {}".format(min_sum, result))
+
+    return min_sum + divide_k_biggest_ones(result, k)
