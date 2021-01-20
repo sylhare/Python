dbffe98f707348388e67d6bb928e03630cc925c1
sylhare <sylhare@outlook.com>
2021-01-20 12:16:28 -0500

    Add test_template

---
+++ src/projects/Interview/tests/test_template.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+class RandomTest(unittest.TestCase):
+
+    def setUp(self):
+        """Initialisation des tests."""
+
+    def test_example_0(self):
+        excepted = ""
+        actual = ""
+        self.assertEqual(excepted, actual)
+
+    def test_example_1(self):
+        boolean = True
+        self.assertTrue(boolean)
+
+    def test_example_2(self):
+        boolean = False
+        self.assertFalse(boolean)
+
+    def test_example_3(self):
+        actual_list = [""]
+        expected_element = ""
+        self.assertIn(expected_element, actual_list)
+
+
+if __name__ == "__main__":
+    unittest.main()
